654ab0ba315a04a7406e47324196b96bd4c1f9bf
ganesh-tyjo <ganeshshinde.tyjo@gmail.com>
2021-06-26 19:46:09 +0530

    Add quote in web page

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -16,13 +16,30 @@ _.forEach([...numbers], (item) => console.log(item));
 document.querySelector('#get-joke').addEventListener('click', (e) => {
   const jokeType = document.querySelector('#hear-joke #joke-type').value;
 
+  //Make button disabled until joke fetching is completed
+  document.querySelector('#get-joke').disabled = true;
+
   // Get joke from API and display it on page
+  getJoke(jokeType);
+});
+
+const getJoke = (jokeType) => {
   joke
     .getJoke(jokeType)
     .then((joke) => {
       document.querySelector(
         '#joke'
-      ).innerHTML = `<div class="setup">${joke.setup.trim()}</div><br /><div class="punchline">${joke.punchline.trim()}</div><br />`;
+      ).innerHTML = `<div class="setup">${joke.setup.trim()}</div><br /><div class="punchline">${joke.punchline.trim()}</div>`;
+
+      //Enable button
+      document.querySelector('#get-joke').disabled = false;
     })
-    .catch((error) => (document.querySelector('#joke').innerHTML = error));
-});
+    .catch((error) => {
+      document.querySelector('#joke').innerHTML = 'Something went wrong !!';
+
+      //Enable button
+      document.querySelector('#get-joke').disabled = false;
+    });
+};
+
+getJoke('general');
